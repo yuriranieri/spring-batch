3cfbd216043afc3f229d4b2725952f660c0eaa5c
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-08 09:34:22 -0300

    aula 21 externalização de propriedades

---
+++ src/main/java/com/example/cursospringbatch/config/PropsConfig.java
@@ -0,0 +1,18 @@
+package com.example.cursospringbatch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer config(){
+        var configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource("C:\\Users\\yuri.ranieri\\springBatch\\config\\application.properties"));
+        return configurer;
+    }
+
+}
